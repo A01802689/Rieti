b07c79b1a6ecfeaa9229f72ca8190c44b8da2f2c
svjuanma <jsanchezvalaz@gmail.com>
2026-09-05 18:08:56 -0600

    Dashboard and API init

---
+++ RietiAPI/README.md
@@ -0,0 +1,5 @@
+# Rieti's API component
+## Running
+**Test:** ```npm run test ``` <br>
+**Dev mode:** ```npm run dev``` <br>
+**Build:** ```docker build .``` <br>
---
+++ RietiAPI/src/index.test.ts
@@ -0,0 +1,8 @@
+// Example of testing with vitet and ts
+import { describe, it, expect } from "vitest";
+
+describe("suma", () => {
+  it("suma dos números", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
